ef603452b9ae1a881fed61847abdaa2f7f5183be
AnhDThe.se <anhdthe187386@fpt.edu.vn>
2025-10-10 07:22:30 +0700

    Add simple orc AI Behavior
    Orc can move around, have idle and walk time countdown,
    When player in range orc will attack

---
+++ Assets/Script/Enemy/AttackDetection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackDetection : MonoBehaviour
+{
+    [SerializeField] private PatrollPhysics patrollPhysics;
+
+    //Vào vùng phát hiện - attackable
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        patrollPhysics.inAttackRange = true;
+    }
+
+    //Thoát khỏi vùng phát hiện
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        patrollPhysics.inAttackRange = false;
+    }
+}
---
+++ Assets/Script/Enemy/PatrollPhysics.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrollPhysics : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    [Header("Detech Ground And Wall")]
+    [SerializeField] private float checkRadius;
+    [SerializeField] private Transform wallCheckPoint;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private LayerMask whatToDetect;
+    public bool groundDetected;
+    public bool wallDetected;
+
+    [Header("Colliders")]
+    [SerializeField] private BoxCollider2D attackDetectionCol;
+    public bool inAttackRange;
+
+    private void FixedUpdate()
+    {
+        groundDetected = Physics2D.OverlapCircle(groundCheckPoint.position, checkRadius, whatToDetect);
+        wallDetected = Physics2D.OverlapCircle(wallCheckPoint.position, checkRadius, whatToDetect);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheckPoint.position, checkRadius);
+        Gizmos.DrawWireSphere(wallCheckPoint.position, checkRadius);
+    }
+
+    //Đứng yên để hành động
+    public void NegateForces()
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
