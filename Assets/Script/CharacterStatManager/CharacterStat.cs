f59e4045c6ccde254aca8e70bc5ff351f9813174
kietpthe182092 <phamkiet29xt@gmail.com>
2025-11-04 09:08:09 +0700

    add health ui, no fix yet

--- Assets/Script/CharacterStatManager/CharacterStat.cs
+++ Assets/Script/CharacterStatManager/CharacterStat.cs
@@ -1,3 +1,57 @@
+//using System.Collections;
+//using UnityEngine;
+
+//public class CharacterStat : MonoBehaviour
+//{
+//    [Header("Stat")]
+//    [SerializeField] public int maxHealth;
+//    [SerializeField] public int damage;
+//    private int currentHealth;
+//    private Animator animator;
+//    public bool isDead = false;
+
+//    void Start()
+//    {
+//        animator = GetComponent<Animator>();
+//        currentHealth = maxHealth;
+//    }
+
+//    public void TakeDamage(int dame)
+//    {
+//        if (isDead) return;
+//        currentHealth -= dame;
+
+//        if (currentHealth <= 0)
+//        {
+//            isDead = true;
+//            animator.SetTrigger("Die");
+//            if (AudioManager.Instance != null)
+//                AudioManager.Instance.PlayPlayerDeath();
+//        }
+//        else
+//        {
+//            animator.SetTrigger("Hurt");
+//            if (AudioManager.Instance != null)
+//                AudioManager.Instance.PlayPlayerHurt();
+//        }
+//    }
+
+//    public void Attack(CharacterStat stat)
+//    {
+//        if (isDead) return;
+//        stat.TakeDamage(damage);
+//    }
+
+//    public void RestoreHealth()
+//    {
+//        currentHealth = maxHealth;
+//        isDead = false;
+//        animator.ResetTrigger("Die");
+//        animator.ResetTrigger("Hurt");
+//    }
+//}
+
+
 using System.Collections;
 using UnityEngine;
 
@@ -6,31 +60,47 @@ public class CharacterStat : MonoBehaviour
     [Header("Stat")]
     [SerializeField] public int maxHealth;
     [SerializeField] public int damage;
+
     private int currentHealth;
     private Animator animator;
     public bool isDead = false;
+
+    [Header("UI")]
+    public HealthBar healthBar; // gan trong Inspector
 
     void Start()
     {
         animator = GetComponent<Animator>();
         currentHealth = maxHealth;
+
+        if (healthBar != null)
+            healthBar.UpdateBar(currentHealth, maxHealth);
     }
 
     public void TakeDamage(int dame)
     {
         if (isDead) return;
+
         currentHealth -= dame;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        // Cap nhat thanh mau 
+        if (healthBar != null)
+            healthBar.UpdateBar(currentHealth, maxHealth);
 
         if (currentHealth <= 0)
         {
             isDead = true;
             animator.SetTrigger("Die");
+            StartCoroutine(RemoveAfterAni());
+
             if (AudioManager.Instance != null)
                 AudioManager.Instance.PlayPlayerDeath();
         }
         else
         {
             animator.SetTrigger("Hurt");
+
             if (AudioManager.Instance != null)
                 AudioManager.Instance.PlayPlayerHurt();
         }
@@ -42,11 +112,17 @@ public class CharacterStat : MonoBehaviour
         stat.TakeDamage(damage);
     }
 
-    public void RestoreHealth()
+    private IEnumerator RemoveAfterAni()
     {
-        currentHealth = maxHealth;
+        float length = animator.GetCurrentAnimatorClipInfo(0).Length;
+        yield return new WaitForSeconds(length);
+        Destroy(gameObject);
+    }
+    public void RestoreHealth()
+   {
+       currentHealth = maxHealth;
         isDead = false;
         animator.ResetTrigger("Die");
         animator.ResetTrigger("Hurt");
-    }
+   }
 }
---
+++ Assets/Script/Health/HealthBar.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image fillBar;
+    public TextMeshProUGUI valueText;
+
+    public void UpdateBar(int currentValue, int maxValue)
+    {
+        fillBar.fillAmount = (float)currentValue / (float)maxValue;
+        valueText.text = $"{currentValue} / {maxValue}";
+    }
+}
