9819470536444f19245d129225d630089d0ccae1
Truong <truongdhhe187014@fpt.edu.vn>
2025-11-11 01:41:31 +0700

    add victory scene

---
+++ Assets/Script/intro/MainMenuButton.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour
+{
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
