698e8c06f2408c916b46f03357f7ed29d84ced7e
Truong <truongdhhe187014@fpt.edu.vn>
2025-11-12 12:51:44 +0700

    fix select character

--- Assets/Script/CharactorSelection/CharacterSelect.cs
+++ Assets/Script/CharactorSelection/CharacterSelect.cs
@@ -1,11 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Security.Cryptography.X509Certificates;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 // using Color = UnityEngine.Color;
 
 
@@ -14,20 +10,34 @@ public class CharacterSelect : MonoBehaviour
     private int index;
     private int selectedIndex;
     [SerializeField] Transform container;
-    [SerializeField] GameObject[] charactor;
+    [SerializeField] GameObject[] characterPrefabs;
+    private GameObject[] charactor;
     private float moveDuration = 0.05f;
     [SerializeField] private AnimationCurve ease;
     [SerializeField] private bool useUnscaledTime = true;
     private Coroutine moveCo;
     private Vector3 containerStartPos;
     [SerializeField] TextMeshProUGUI characterName;
+    [SerializeField] Transform parentObject;
     // Start is called before the first frame update
     void Start()
     {
         index = 0;
         selectedIndex = 0;
+        charactor = new GameObject[characterPrefabs.Length];
+        for (int i = 0; i < characterPrefabs.Length; i++)
+        {
+            GameObject clone = Instantiate(characterPrefabs[i], parentObject);
+            Rigidbody2D rb2D = clone.GetComponent<Rigidbody2D>();
+            if (rb2D != null) rb2D.isKinematic = true;
+            clone.transform.localPosition = new Vector3(i * 6, 0, 0);
+            charactor[i] = clone;
+            
+        }
+        container.localPosition = Vector3.zero;
+
+        containerStartPos = container.localPosition;
         SelectCharactor();
-        containerStartPos = container.transform.position;
     }
 
     // Update is called once per frame
@@ -43,6 +53,7 @@ public class CharacterSelect : MonoBehaviour
             index--;
             SelectCharactor();
             selectedIndex = index;
+            
         }
     }
 
@@ -64,7 +75,7 @@ public class CharacterSelect : MonoBehaviour
             {
                 charactor[i].GetComponent<SpriteRenderer>().color = Color.white;
                 charactor[i].GetComponent<Animator>().enabled = true;
-                characterName.text =  charactor[i].name;
+                characterName.text =  charactor[i].name.Replace("(Clone)", "");;
 
                 if (i == 0 && selectedIndex == 0)
                 {
@@ -73,7 +84,8 @@ public class CharacterSelect : MonoBehaviour
                 else
                 {
                     Vector3 targetPos = containerStartPos - new Vector3(i * 6f, 0f, 0f);
-                    StartMove(targetPos);
+                    StartMoveLocal(targetPos);
+                    
                 }
             }
             else
@@ -84,27 +96,28 @@ public class CharacterSelect : MonoBehaviour
         }
     }
 
-    private void StartMove(Vector3 worldTarget)
+    private void StartMoveLocal(Vector3 localTarget)
     {
         if (moveCo != null) StopCoroutine(moveCo);
-        moveCo = StartCoroutine(MoveTo(container, worldTarget, moveDuration, ease, useUnscaledTime));
+        moveCo = StartCoroutine(MoveToLocal(container, localTarget, moveDuration, ease, useUnscaledTime));
     }
 
-    private static IEnumerator MoveTo(Transform tf, Vector3 target, float duration, AnimationCurve ease, bool useUnscaled)
+    private static IEnumerator MoveToLocal(Transform tf, Vector3 targetLocal, float duration, AnimationCurve ease, bool useUnscaled)
     {
         if (tf == null) yield break;
         if (ease == null) ease = AnimationCurve.EaseInOut(0, 0, 1, 1);
-        Vector3 start = tf.position;
+        Vector3 start = tf.localPosition;
         float t = 0f;
         while (t < 1f)
         {
             t += (useUnscaled ? Time.unscaledDeltaTime : Time.deltaTime) / duration;
             float k = ease.Evaluate(Mathf.Clamp01(t));
-            tf.position = Vector3.LerpUnclamped(start, target, k);
+            tf.localPosition = Vector3.LerpUnclamped(start, targetLocal, k);
             yield return null;
         }
-        tf.position = target;
+        tf.localPosition = targetLocal;
     }
+
     
     public void PlayGame()
     {
